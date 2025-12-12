531db127a82de3e765c5ce5ed081c7996598f9be
Sreejib-Nandy <sreejibnandy2518@gmail.com>
2025-12-13 01:14:31 +0530

    New Change

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -95,8 +95,8 @@ export const login = async(req, res) => {
 
         res.cookie("token", token, {
             httpOnly : true,
-            secure : process.env.NODE_ENV === "production",
-            sameSite: "lax",
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
             maxAge: 24 * 60 * 60 * 1000
         })
 
